fb193b84a03fd378bd489ca4035074f70ea9e680
technbuzz <samiullah.khan.1989@gmail.com>
2017-03-13 02:07:15 +0500

    trying to fix database access issue

--- server.js
+++ server.js
@@ -127,5 +127,5 @@ app.post('/cityList', function(req, res){
   })
 })
 
-app.listen(process.env.PORT || 3000);
-console.log('Server running on port 3000...');
+app.listen(process.env.PORT || 5000);
+console.log('Server running on port 5000...');
